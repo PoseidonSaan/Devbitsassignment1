2ae0371ef7dd2c551cd9b6691e87a0eef7e2fad6
PoseidonSaan <ojasvi.tiwari.mec21@itbhu.ac.in>
2023-04-01 00:07:36 +0530

    add

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -58,7 +58,7 @@ const NavBar = () => {
         <ul className='flex flex-col items-left text-left absolute top-0 left-0 w-full h-screen bg-stone-800 text-white'>
           <li className='w-28 ml-11'><img src="https://www.logogarden.com/wp-content/uploads/2020/09/mortgage-broker-logo-2.png" alt="logo" /></li>
           <li className='cursor-pointer uppercase font-small
-              hover:scale-105 duration-200 py-2 px-20 mt-20 focus:text-yellow-400'><a href='/home' alt="home">About us</a></li>
+              hover:scale-105 duration-200 py-2 px-20 mt-20 focus:text-yellow-400'><a href='/' alt="home">About us</a></li>
           <li className=' cursor-pointer uppercase font-small
               hover:scale-105 duration-200 py-2 px-20 focus:text-yellow-400'><a href='/dashboard' alt="Dashboard">Dashboard</a></li>
           <li className='px-20 py-2 cursor-pointer uppercase font-small
